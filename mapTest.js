04b1501d7336caaccc2c98b93e7bcc7844515218
send2nate <nateoakley@gmail.com>
2018-08-07 22:13:33 -0700

    Initial commit

---
+++ mapTest.js
@@ -0,0 +1,9 @@
+console.log("connected");
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "350px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+} 

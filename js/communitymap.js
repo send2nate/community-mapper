c096c279941f4fc65f9145ce3e2cee4ed70d4fc3
send2nate <nateoakley@gmail.com>
2018-08-12 22:33:55 -0700

    Add additional map layer toggle buttons and functionality.

--- js/communitymap.js
+++ js/communitymap.js
@@ -19,3 +19,43 @@ $("#node-layer").click(function(event) {
         $(this).addClass('selected');
    }
 });
+
+$("#primary-trail-layer").click(function(event) {
+    event.preventDefault();
+    if(map.hasLayer(layer_MCP_PrimaryTrailway10_7)) {
+        $(this).removeClass('selected');
+        map.removeLayer(layer_MCP_PrimaryTrailway10_7);
+    } else {
+        map.addLayer(layer_MCP_PrimaryTrailway10_7);        
+        $(this).addClass('selected');
+   }
+});
+
+$("#secondary-trail-layer").click(function(event) {
+    event.preventDefault();
+    if(map.hasLayer(layer_MCP_ProposedSecondaryDGTrail_8)) {
+        $(this).removeClass('selected');
+        map.removeLayer(layer_MCP_ProposedSecondaryDGTrail_8);
+    } else {
+        map.addLayer(layer_MCP_ProposedSecondaryDGTrail_8);        
+        $(this).addClass('selected');
+   }
+});
+
+
+$(".bike-lane-layer").click(function(event) {
+    event.preventDefault();
+    if(map.hasLayer(layer_MCP_ProposedBikePathClassI_6) || map.hasLayer(layer_MCP_BikeLaneClassII_3) || map.hasLayer(layer_MCP_BikeLaneClassIII_2)) {
+        $(this).removeClass('selected');
+        map.removeLayer(layer_MCP_ProposedBikePathClassI_6);
+        map.removeLayer(layer_MCP_BikeLaneClassII_3); 
+        map.removeLayer(layer_MCP_BikeLaneClassIII_2);
+    } else {
+        map.addLayer(layer_MCP_ProposedBikePathClassI_6) ;
+        map.addLayer(layer_MCP_BikeLaneClassII_3);
+        map.addLayer(layer_MCP_BikeLaneClassIII_2);        
+        $(this).addClass('selected');
+   }
+});
+
+
